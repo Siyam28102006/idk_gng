9a474e34c175784221be56d65abd26f5b43bcd28
rohan9077 <istiak9077@gmail.com>
2026-09-18 23:30:05 +0600

    Add files via upload

---
+++ core.cpp
@@ -0,0 +1,16 @@
+// OUR Target is next Hackathon 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define dd double
+
+int main(){
+
+    ios::sync_with_stdio(0);cin.tie(0);
+    
+    cout << "idk_gng" << endl; 
+    
+    return 0;
+}
